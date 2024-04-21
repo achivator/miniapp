632325b2f5bd62919516391f25868011defd0a89
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2024-04-21 19:02:37 +0200

    fix missing new

--- src/app/api/achievement/route.js
+++ src/app/api/achievement/route.js
@@ -5,7 +5,7 @@ export const dynamic = "force-dynamic"; // defaults to auto
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const user_id = parseInt(searchParams.get("user_id"));
-  const _id = ObjectId(searchParams.get("_id"));
+  const _id = new ObjectId(searchParams.get("_id"));
 
   const mongo = new MongoClient(process.env.MONGODB_URI);
   await mongo.connect();
